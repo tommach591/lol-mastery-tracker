9181876bbafa2e96e8e5e7df7dcd89c9c1ad3607
Tom Mach <57111473+tommach591@users.noreply.github.com>
2023-03-17 17:34:38 -0700

    added footer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,6 +8,13 @@ function Home() {
         <h1>LoL Mastery Tracker</h1>
       </div>
       <Searchbar />
+      <h2 className="Footer">
+        LoL Mastery Tracker is not endorsed by Riot Games and does not reflect
+        the views or opinions of Riot Games or anyone officially involved in
+        producing or managing Riot Games properties. Riot Games and all
+        associated properties are trademarks or registered trademarks of Riot
+        Games, Inc.
+      </h2>
     </div>
   );
 }
